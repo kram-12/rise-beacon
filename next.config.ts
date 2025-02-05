41016f7db20eab464906743a5f5674e630ab0dd1
Kalyanram P <kalyanram.p.12@gmail.com>
2025-02-05 23:52:34 +0530

    report page added

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   env:{
     DATABASE_URL: process.env.DATABASE_URL,
     WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
+    NEXT_PUBLIC_GEMINI_API_KEY : process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY : process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   }
 };
 
